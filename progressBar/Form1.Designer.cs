d26cfe6a13767bcdda88bc481081c764f2e1ad72
Reception2 <Reception2@DUNLRECEPTILL>
2022-10-20 08:41:27 +0100

    Add project files.

---
+++ progressBar/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace progressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.bgWorder = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 125);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 40);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(160, 125);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(109, 40);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // pBar
+            // 
+            this.pBar.Location = new System.Drawing.Point(12, 202);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(257, 15);
+            this.pBar.TabIndex = 2;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(275, 193);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(0, 24);
+            this.lblDisplay.TabIndex = 3;
+            // 
+            // bgWorder
+            // 
+            this.bgWorder.WorkerReportsProgress = true;
+            this.bgWorder.WorkerSupportsCancellation = true;
+            this.bgWorder.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorder_DoWork);
+            this.bgWorder.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorder_ProgressChanged);
+            this.bgWorder.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorder_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Progress Bar Test";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Progress Bar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar pBar;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.ComponentModel.BackgroundWorker bgWorder;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ progressBar/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace progressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(!bgWorder.IsBusy)
+            {
+                bgWorder.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Please wait until finished", "worker running", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(bgWorder.IsBusy)
+            {
+                bgWorder.CancelAsync();
+            }
+        }
+
+        private void bgWorder_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int sum = 0;
+            for(int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(100);
+                sum++;
+                bgWorder.ReportProgress(i);
+                if(bgWorder.CancellationPending)
+                {
+                    e.Cancel = true;
+                    bgWorder.ReportProgress(0);
+                    return;
+                }
+
+                e.Result = sum;
+            }
+        }
+
+        private void bgWorder_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pBar.Value = e.ProgressPercentage;
+            lblDisplay.Text = e.ProgressPercentage.ToString()+ "%";
+        }
+
+        private void bgWorder_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if(e.Cancelled)
+            {
+                lblDisplay.Text = "Progress Cancelled";
+            }
+            else if(e.Error != null)
+            {
+                lblDisplay.Text = e.Error.Message;
+            }
+            else
+            {
+                lblDisplay.Text = "Total " + e.Result.ToString() + "%";
+            }
+        }
+    }
+}
